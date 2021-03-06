f13ac2542d842a20fe0a9adc2e39a473519fb9ef
George Spanos <gespa11019@gmail.com>
2021-03-06 15:00:14 +0200

    initial commit

---
+++ apps/learn-formly/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'learn-formly-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'learn-formly';
+}
